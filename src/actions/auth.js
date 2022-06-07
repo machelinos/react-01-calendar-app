a99d25005ce3461e6056d1002247ce723c40ad62
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-06-06 23:17:52 -0500

    Auth - Login authentication and saving data to localstorage and state

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,11 +1,29 @@
+import { fetchSinToken } from "../helpers/fetch";
 import { types } from "../types/types";
 
 export const startLogin = (email, password) => {
-    return async() => {
-        console.log(email, password);
+    return async( dispatch ) => {
+        try {
+            const resp = await fetchSinToken('auth',{email, password},'POST');
 
-        return {
-            type: types.authStartLogin
+            if(!resp.ok) throw resp;
+
+            const body = await resp.json();
+            localStorage.setItem('token', body.token)
+            localStorage.setItem('token-init-date', new Date().getTime());
+
+            dispatch(login({uid: body.uid, name:body.name}))
+
+            
+        } catch (error) {
+            console.log(await error.json());
         }
+    }
+}
+
+const login = (user) => {
+    return {
+        type: types.authLogin,
+        payload: user
     }
 }
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -10,6 +10,12 @@ export const authReducer = (state=initialState, action) => {
     switch (action.type) {
         case types.authStartLogin:
             return state;
+        case types.authLogin:
+            return {
+                ...state,
+                checking: false,
+                ...action.payload
+            }
         default:
             return state;
     }
